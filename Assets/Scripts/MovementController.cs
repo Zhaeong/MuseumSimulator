0caeae7ecf51beba80ca8309386afe4a89734762
Zhaeong <owen_zzz@hotmail.com>
2017-05-14 21:03:24 -0400

    a working fps controller with character controller
    also works with gravity

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -8,6 +8,8 @@ public class MovementController : MonoBehaviour {
     public float smoothSpeed;
     public float MovementSpeed;
     public float HorizRotationSpeed, VerticalRotationSpeed;
+
+    public float gravity;
     private float yaw, pitch;
 
 
@@ -15,13 +17,21 @@ public class MovementController : MonoBehaviour {
 
     private Vector3 velocity = Vector3.zero;
 
-    //private CharacterController CC;
+    private float slerpVal;
+
+    private bool isMoving;
+    private Vector3 Movement = Vector3.zero;
+
+    private CharacterController CC;
+
     // Use this for initialization
     void Start () {
-        //CC = gameObject.GetComponent<CharacterController>();
+        CC = gameObject.GetComponent<CharacterController>();
         mainCamera = Camera.main;
         yaw = 0.0f;
         pitch = 0.0f;
+
+        slerpVal = 0;
     }
 	
 	// Update is called once per frame
@@ -46,58 +56,72 @@ public class MovementController : MonoBehaviour {
         //Debug.DrawRay(transform.position, LeftNormalized, Color.red);
         //Debug.DrawRay(transform.position, (ForwardNormalized + LeftNormalized).normalized, Color.green);
 
-        Vector3 Movement = Vector3.zero;
+        //
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            isMoving = true;
 
-        //The if statements specify the direction that it will move in
-        //Move Forward
-        if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") == 0)
-        {
-            Movement = LeftNormalized;
-        }
-        //Move Backward
-        else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") == 0)
-        {
-            Movement = -LeftNormalized;
-        }
-        //Move Left
-        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") > 0)
-        {
-            Movement = ForwardNormalized;
-        }
-        //Move Right
-        else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") < 0)
-        {
-            Movement = -ForwardNormalized;
-        }
-        //Move Forward Left
-        else if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") > 0)
-        {
-            Movement = BothNormalized;
-        }
-        //Move Forward Right
-        else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") < 0)
-        {
-            Movement = -BothNormalized;
-        }
-        //Move Backward Left
-        else if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") < 0)
-        {
+            if (slerpVal < 1)
+            {
+                slerpVal += Time.deltaTime * smoothSpeed;
+            }            
 
-            Vector3 newMovement = Vector3.Reflect(BothNormalized, ForwardNormalized);
-            Movement = newMovement;
-        }
-        //Move Backward Right
-        else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") > 0)
-        {
-            Vector3 newMovement = Vector3.Reflect(BothNormalized, LeftNormalized);
-            Movement = newMovement;
-        }
+            //The if statements specify the direction that it will move in
+            //Move Forward
+            if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") == 0)
+            {
+                Movement = LeftNormalized;
+            }
+            //Move Backward
+            else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") == 0)
+            {
+                Movement = -LeftNormalized;
+            }
+            //Move Left
+            else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") > 0)
+            {
+                Movement = ForwardNormalized;
+            }
+            //Move Right
+            else if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") < 0)
+            {
+                Movement = -ForwardNormalized;
+            }
+            //Move Forward Left
+            else if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") > 0)
+            {
+                Movement = BothNormalized;
+            }
+            //Move Forward Right
+            else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") < 0)
+            {
+                Movement = -BothNormalized;
+            }
+            //Move Backward Left
+            else if (Input.GetAxis("Horizontal") > 0 && Input.GetAxis("Vertical") < 0)
+            {
+                Vector3 newMovement = Vector3.Reflect(BothNormalized, ForwardNormalized);
+                Movement = newMovement;
+            }
+            //Move Backward Right
+            else if (Input.GetAxis("Horizontal") < 0 && Input.GetAxis("Vertical") > 0)
+            {
+                Vector3 newMovement = Vector3.Reflect(BothNormalized, LeftNormalized);
+                Movement = newMovement;
+            }
+        }        
         else
         {
-            Movement = Vector3.zero ;
-        }
+            Movement = Vector3.zero;
+            slerpVal = 0;
+            if (slerpVal > 0)
+            {
+                slerpVal -= Time.deltaTime * smoothSpeed;
+            }
+            
+        }     
 
-        Vector3 DirectionToMove = transform.position + Movement;
+        //Vector3 DirectionToMove = transform.position + Movement;
 
         //Debug.DrawRay(transform.position, new Vector3(BothNormalized.x , BothNormalized.y, BothNormalized.z), Color.yellow);
         //Debug.DrawRay(transform.position, new Vector3(-BothNormalized.x, -BothNormalized.y, -BothNormalized.z), Color.green);
@@ -106,8 +130,12 @@ public class MovementController : MonoBehaviour {
 
         Debug.DrawRay(transform.position, Movement, Color.black);
 
-        transform.position = Vector3.SmoothDamp(transform.position, DirectionToMove, ref velocity, smoothSpeed);
+        //transform.position = Vector3.SmoothDamp(transform.position, DirectionToMove, ref velocity, smoothSpeed);
 
-        //CC.Move(Movement * MovementSpeed *  Time.deltaTime);
+        
+        Movement.y = -gravity;
+
+        CC.Move(Vector3.Slerp(new Vector3(0,-gravity, 0), Movement, slerpVal) * MovementSpeed * Time.deltaTime);
+        
     }
 }
