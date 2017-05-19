7f3c05fc1a0cec0d7ce59b88e3e697ec5c167b93
Zhaeong <owen_zzz@hotmail.com>
2017-05-19 00:00:14 -0400

    added some stuff

---
+++ Assets/Scripts/AnchorController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnchorController : MonoBehaviour {
+
+    public GameObject Anchor;
+    public float anchorSpeed = 5;
+    public GameObject AnchorSpawnLocation;
+    public GameObject AnchorEndLocation;
+
+    private InterfaceModule IM;
+
+    private GameObject AnchorClone;
+    private bool AnchorExist;
+    private bool MoveAnchor;
+	// Use this for initialization
+	void Start () {
+        IM = gameObject.GetComponent<InterfaceModule>();
+        MoveAnchor = false;
+        AnchorExist = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (IM.DoAction)
+        {
+            
+            if (!AnchorExist)
+            {
+                CreateAnchor();
+                MoveAnchor = true;
+                
+            }
+            
+            IM.DoAction = false;
+        }
+
+        if (MoveAnchor)
+        {
+            AnchorClone.transform.position = Vector3.MoveTowards(AnchorClone.transform.position, AnchorEndLocation.transform.position, anchorSpeed);
+            AnchorClone.transform.Rotate(new Vector3(0, 0, 1));
+            if (AnchorClone.transform.position.y >= AnchorEndLocation.transform.position.y)
+            {
+                Destroy(AnchorClone);
+                MoveAnchor = false;
+                AnchorExist = false;
+            }
+
+        }
+    }
+
+    void CreateAnchor()
+    {
+        AnchorClone = Instantiate(Anchor, AnchorSpawnLocation.transform.position, Quaternion.Euler(-90, 0, 0));
+        AnchorExist = true;
+    }
+
+    
+
+
+}
---
+++ Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceController : MonoBehaviour
+{
+    public Texture2D crosshairImage;
+
+    public float DistanceToFind = 20;
+
+    RaycastHit hitInfo;
+    private string sObject;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * DistanceToFind, Color.red);
+
+        Ray rayTosend = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if (Physics.Raycast(rayTosend, out hitInfo, DistanceToFind))
+        {
+            GameObject HitObj = hitInfo.transform.gameObject;
+            sObject = HitObj.name;
+            if (HitObj.tag == "Button")
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    HitObj.GetComponent<InterfaceModule>().DoAction = true;
+                }
+            }
+
+        }
+        else
+        {
+            sObject = null;
+        }
+
+        
+    }
+    void OnGUI()
+    {
+        float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
+        float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
+
+        GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
+
+        GUI.Label(new Rect(10, 10, 100, 20), sObject);
+    }
+}
